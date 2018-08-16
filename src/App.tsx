7f90a4af6234f8e75b23a353d090d78ded55d107
oniipon <oniipon@gmail.com>
2018-08-16 15:27:36 +0900

    styled-commponentsを試してみた

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
 import * as React from "react";
+import styled from "styled-components";
 import "./App.css";
 import Container from "./containers/sampleContainer";
-
+import { COLORS, FONT } from "./css";
 class App extends React.Component {
   public render() {
+    const Hoge = styled.button`
+      font-size: ${FONT.SIZE.LARGE};
+      width: ${100 + 100}px;
+      height: 200px;
+    `;
+    const DivElement = styled.div`
+      background-color: ${COLORS.main};
+    `;
+    // tslint:disable-next-line:no-console
+    console.log(Hoge);
     return (
       <div className="App">
-      <h1> reduxのサンプルらしい </h1>
+        <h1> reduxのサンプルらしい </h1>
         <Container />
+        <DivElement>
+          <Hoge>aaaaaaaa</Hoge>
+        </DivElement>
       </div>
     );
   }
---
+++ src/css.ts
@@ -0,0 +1,8 @@
+export const FONT = {
+  SIZE: { SMALL: 10, MEDIUM: 20, LARGE: 30 }
+};
+
+export const COLORS = {
+  main: "red",
+  sub: "black,"
+};
